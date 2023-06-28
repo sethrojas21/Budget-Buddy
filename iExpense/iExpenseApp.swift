658e493f32d93f0672ef21127a1fb1f3b9e655c0
sethrojas21 <70863997+sethrojas21@users.noreply.github.com>
2023-06-28 09:02:16 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Seth Rojas on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
